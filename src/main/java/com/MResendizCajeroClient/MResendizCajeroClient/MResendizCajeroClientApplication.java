21f70a27bb4e2df03e218c0b9e93803b61c16b9e
digis <digis@ALIEN18>
2026-01-20 17:43:24 -0600

    Creacion del proyecto y primer commit

---
+++ src/main/java/com/MResendizCajeroClient/MResendizCajeroClient/MResendizCajeroClientApplication.java
@@ -0,0 +1,13 @@
+package com.MResendizCajeroClient.MResendizCajeroClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MResendizCajeroClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MResendizCajeroClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MResendizCajeroClient/MResendizCajeroClient/MResendizCajeroClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MResendizCajeroClient.MResendizCajeroClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MResendizCajeroClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
